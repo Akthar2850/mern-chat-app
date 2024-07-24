f27a449f2764af4409e829a56a3eb21bec42cf31
Akthar2850 <syedakram2850@gmail.com>
2024-07-24 17:12:43 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,9 +12,9 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from './socket/socket.js';
 
-
-
 const PORT = process.env.port || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,14 +25,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-
- /* app.get("/", (req, res) => {
-    root route http:// localhost:5000/
-    res.send("Hello World!!");
-}); 
- */
 
 
 server.listen(PORT,() => {
